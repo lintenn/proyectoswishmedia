3315cf0fffcb9b2eb5ced99ccb3255f9b744bbeb
Galo <galopg2001@hotmail.com>
2021-05-22 00:24:34 +0200

    Validación correo introduce

--- prSwishMedia/src/prSwishMedia/Listeners/MouseClick.java
+++ prSwishMedia/src/prSwishMedia/Listeners/MouseClick.java
@@ -1,14 +1,22 @@
 package prSwishMedia.Listeners;
 
+import prSwishMedia.Controllers.ConfirmedController;
 import prSwishMedia.Main;
 import prSwishMedia.Views.ConfirmedView;
+import prSwishMedia.Views.LoginView;
 
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 public class MouseClick implements MouseListener {
 
-    ConfirmedView cv=new ConfirmedView();
+    ConfirmedView cv;
+    LoginView lv;
+
+    public MouseClick(LoginView l, ConfirmedView c){
+        cv=c;
+        lv=l;
+    }
 
     @Override
     public void mouseClicked(MouseEvent e) {
@@ -24,6 +32,9 @@ public class MouseClick implements MouseListener {
     public void mouseReleased(MouseEvent e) {
         Main.frame.setContentPane(cv.getPanel());
         Main.frame.setVisible(true);
+
+        //ConfirmedController cc=new ConfirmedController(lv,cv);
+        //cv.controlador(cc);
     }
 
     @Override
