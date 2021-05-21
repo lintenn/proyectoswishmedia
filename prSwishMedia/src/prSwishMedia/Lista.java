71402bf6a7dad478c28eccb6cb0e2d2526858ca4
Angelo <0619974249@uma.es>
2021-05-21 18:34:12 +0200

    toString clase Lista

--- prSwishMedia/src/prSwishMedia/Lista.java
+++ prSwishMedia/src/prSwishMedia/Lista.java
@@ -1,24 +1,33 @@
 package prSwishMedia;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Lista {
-    private ContenidoMultimedia[] contMedia;
+    private List<ContenidoMultimedia> contMedia;
     private int id;
     private String nombre;
     private Date fechaCreacion;
     public int numCont;
 
     public Lista(int id, String nombre, Date fechaCreacion) {
-        contMedia = new ContenidoMultimedia[1];
+        contMedia = new ArrayList<>();
         this.id = id;
         this.nombre = nombre;
         this.fechaCreacion = fechaCreacion;
         numCont=0;
     }
 
-    public ContenidoMultimedia[] getContMedia() {
+    public Lista(List<ContenidoMultimedia> list,int id, String nombre, Date fechaCreacion) {
+        contMedia = list;
+        this.id = id;
+        this.nombre = nombre;
+        this.fechaCreacion = fechaCreacion;
+        numCont=0;
+    }
+
+    public List<ContenidoMultimedia> getContMedia() {
         return contMedia;
     }
 
@@ -51,29 +60,21 @@ public class Lista {
     }
 
     public void añadirContenidoMultimedia(ContenidoMultimedia cM){
-        if(contMedia.length==numCont){
-            contMedia = Arrays.copyOf(contMedia, numCont*2);
-        }
-        contMedia[numCont] = cM;
-        numCont++;
-    }
-
-    private int buscar(ContenidoMultimedia cM){
-        int i=0;
-        while(contMedia[i]!=cM && i<numCont){
-            i++;
-        }
-        return i;
+        contMedia.add(cM);
     }
 
     public void borrarContenidoMultimedia(ContenidoMultimedia cM){
-        int i=buscar(cM);
-        if(i<numCont){
-            while(i<numCont-1){
-                contMedia[i]=contMedia[i+1];
-                i++;
-            }
-            numCont--;
-        }
+        contMedia.remove(cM);
     }
+
+    public String toString() {
+        String s;
+
+        s = "Películas y series de la lista " + getNombre() + ":\n";
+
+        for (ContenidoMultimedia contMul : contMedia) {
+            s += contMul.getNombre() + "\n";
+        }
+
+        return s;
 }
