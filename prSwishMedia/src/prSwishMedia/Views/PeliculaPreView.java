5507bc65be87a3592169023b95b73b38ee819587
Galo <galopg2001@hotmail.com>
2021-05-25 01:39:02 +0200

    Muestra de listas POR FIN

--- prSwishMedia/src/prSwishMedia/Views/PeliculaPreView.java
+++ prSwishMedia/src/prSwishMedia/Views/PeliculaPreView.java
@@ -2,11 +2,25 @@ package prSwishMedia.Views;
 
 import javax.swing.*;
 
-public class PeliculaPreView {
+public class PeliculaPreView extends JFrame{
     private JPanel panel1;
-    private JComboBox comboBox1;
+    private JComboBox Listas;
     private JTextPane Sinopsis;
     private JLabel Nombre;
     private JLabel Imagen;
-    private JLabel Duracion;
+    private JLabel Genero;
+    private JLabel Valoración;
+
+    public PeliculaPreView(String nombre, int imagen,String sinopsis,String genero,int valoracion, JComboBox listas){
+        Nombre.setText(nombre);
+        Imagen.setIcon(new ImageIcon( "prSwishMedia/imagen/"+imagen +".jpg"));
+        Listas=listas;
+        Sinopsis.setText(sinopsis);
+        Genero.setText(genero);
+        Valoración.setText(Integer.toString(valoracion));
+    }
+
+    public JPanel getPanel() {
+        return panel1;
+    }
 }
