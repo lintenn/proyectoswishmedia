1857dde8c756097d894a77a3f025bf94aa23b8ba
Alfonso Gonzalez <0619973969@uma.es>
2021-05-26 01:25:36 +0200

    usuariopreview

--- prSwishMedia/src/prSwishMedia/Controllers/UsuarioPreViewController.java
+++ prSwishMedia/src/prSwishMedia/Controllers/UsuarioPreViewController.java
@@ -1,4 +1,26 @@
 package prSwishMedia.Controllers;
 
-public class UsuarioPreViewController {
+import prSwishMedia.Usuario;
+import prSwishMedia.Views.UsuarioPreView;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class UsuarioPreViewController implements ActionListener {
+
+    private UsuarioPreView pvuser;
+    private Usuario user;
+
+    public UsuarioPreViewController(UsuarioPreView pvu,Usuario u){
+        pvuser = pvu;
+        user = u;
+        pvuser.setDescripción(u.getDescripcion());
+        pvuser.setNombre(u.getNombre());
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
--- prSwishMedia/src/prSwishMedia/Views/UsuarioPreView.java
+++ prSwishMedia/src/prSwishMedia/Views/UsuarioPreView.java
@@ -1,10 +1,19 @@
 package prSwishMedia.Views;
 
 import javax.swing.*;
+import java.awt.event.ActionListener;
 
-public class UsuarioPreView {
+public class UsuarioPreView extends JFrame{
     private JLabel Nombre;
     private JTextPane Descripción;
     private JPanel panel1;
     private JButton eliminarAmigo;
+
+    public UsuarioPreView(){ }
+    public JPanel getPanel() {
+        return panel1;
+    }
+
+    public void setDescripción(String descripción) { Descripción.setText(descripción);}
+    public void setNombre(String nombre) { Nombre.setText(nombre); }
 }
