3bb4ca221389e75e2501a8a24dcee836f5bfe42c
Galo <galopg2001@hotmail.com>
2021-06-01 23:25:14 +0200

    Este funciona

--- prSwishMedia/src/prSwishMedia/Controllers/PeliculaPreViewController.java
+++ prSwishMedia/src/prSwishMedia/Controllers/PeliculaPreViewController.java
@@ -48,7 +48,6 @@ public class PeliculaPreViewController extends ContenidoMultimediaPreViewControl
                         throwables.printStackTrace();
                     }
                 }
-
                 break;
         }
     }
