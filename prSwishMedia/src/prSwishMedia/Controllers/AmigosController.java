634a6fbcf9dff7eba347e604be36b9cc15a9c0b0
Nicolás Álvarez Romero <nicoromero07@uma.es>
2021-06-02 10:32:06 +0200

    ListaAmigos Profile 1

---
+++ prSwishMedia/src/prSwishMedia/Controllers/AmigosController.java
@@ -0,0 +1,11 @@
+package prSwishMedia.Controllers;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AmigosController implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
---
+++ prSwishMedia/src/prSwishMedia/Views/AmigosView.java
@@ -0,0 +1,17 @@
+package prSwishMedia.Views;
+
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class AmigosView extends JDialog {
+
+    private JPanel panel1;
+
+    public AmigosView(){
+        add(panel1);
+        setSize(250,400);
+    }
+    public void controlador(ActionListener ctr){
+
+    }
+}
