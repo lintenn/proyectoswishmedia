5ccab0fd01233e39563290637c1d251a52a7ff1c
Nicolás Álvarez Romero <nicoromero07@uma.es>
2021-06-04 19:34:26 +0200

    changes numerosdeCosas

--- prSwishMedia/src/prSwishMedia/Controllers/UsuarioPreViewController.java
+++ prSwishMedia/src/prSwishMedia/Controllers/UsuarioPreViewController.java
@@ -51,7 +51,7 @@ public class UsuarioPreViewController implements ActionListener {
                             conexion.executeUpdate("UPDATE Amigo SET eresNuevoAmigo=true where usuario1='"+user.getNombre()+"' and usuario2='"+tu.getNombre()+"'");
                             ResultSet rs3 = conexion.executeQuery("SELECT * FROM Usuario where nombre = '"+user.getNombre()+"';");
                             rs3.next();
-                            conexion.executeUpdate("UPDATE Usuario SET numAmigos="+(rs3.getInt("numAmigos")+1)+" where nombre = '"+tu.getNombre()+"';");
+                            conexion.executeUpdate("UPDATE Usuario SET numAmigos="+(tu.getNumAmigos()+1)+" where nombre = '"+tu.getNombre()+"';");
                         }
                     } else {
                         ResultSet rs = conexion.executeQuery("SELECT * FROM Amigo where usuario1 = '"+user.getNombre()+"' and usuario2 = '"+tu.getNombre()+"';");
